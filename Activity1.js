2217ddcf696b5e6f8de42da8a2d83b63f100f5b3
KenShinX-04 <kenminiphil8519@gmail.com>
2025-08-17 16:00:55 +0800

    Add files via upload

---
+++ Activity1.js
@@ -0,0 +1,45 @@
+//1.
+var name = "Ken Abasolo";
+console.log(name);
+
+//2.
+let age = "21";
+console.log(age);
+//3.
+const PI = 3.1416;
+
+console.log(PI);
+
+//4.
+let isStudent = Number(age) < 25;
+console.log(isStudent);
+
+//5.
+let person = {
+    firstName: "Ken",
+    lastName: "Abasolo",
+    address: {
+        barangay: "Buhangin",
+        city: "Davao City",
+        province: "Davao Del Sur",
+    }
+};
+console.log(person.firstName + " " + person.lastName);
+console.log(person.address.barangay + ", " + person.address.city + ", " + person.address.province);
+
+//6.
+
+let colors = ["red", "blue", "green", "yellow", "blue"];
+
+
+
+
+
+
+
+
+
+
+
+
+
